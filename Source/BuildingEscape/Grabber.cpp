339477bec6abb1d90f0ea71f3230b52d098650d0
MisterClass <misterclass2909@yandex.ru>
2021-01-24 13:05:44 +0300

    Add test debug line

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -1,7 +1,8 @@
 // Copyright Mister Class 2020
 
-#include "GameFramework/PlayerController.h"
+#include "DrawDebugHelpers.h"
 #include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 #include "Grabber.h"
 
 //For code readability with out params
@@ -36,16 +37,31 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	//Get player viewpoint
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
+	
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(		//OUT is used only for readability
 		OUT PlayerViewPointLocation, 
 		OUT PlayerViewPointRotation
 	);
 
-	UE_LOG(LogTemp, Warning, TEXT("Player view point location is %s"), 
+	//Calculate line trace end by players look
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+	
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0, 0.5),
+		false,
+		0.f,
+		0,
+		3.f
+	);
+
+	/*UE_LOG(LogTemp, Warning, TEXT("Player view point location is %s"), 
 		*PlayerViewPointLocation.ToString()
 	);
 	UE_LOG(LogTemp, Warning, TEXT("Player view point rotation is %s"), 
 		*PlayerViewPointRotation.ToString()
-	);
+	);*/
 }
 
